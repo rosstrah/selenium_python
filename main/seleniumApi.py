7a234d58dc5512432000f3c449e1e434d658cfba
sareth <sareth_2084@mail.ru>
2023-11-03 15:14:27 +0600

    патч

--- main/seleniumApi.py
+++ main/seleniumApi.py
@@ -6,18 +6,17 @@ from selenium.webdriver.common.by import By
 
 
 class SeleniumApi:
-    __tmpPath = 'tmp/'
+    __tmpPath = 'main/tmp'
 
     def get_screenshot(self, url: str, filename: str):
         options = Options()
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-gpu')
         options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--headless')
         options.add_argument('--start-maximized')
+        options.add_argument('--headless=new')
         result = False
 
-        #options = webdriver.ChromeOptions()
         options.add_extension('main/crx/IIFCHHFNNMPDBIBIFMLJNFJHPIFIFFOG_1_2_13_0.crx')
         #options.add_extension('main/crx/CJPALHDLNBPAFIAMEJDNHCPHJBKEIAGM_1_52_2_0.crx')
 
@@ -48,7 +47,7 @@ class SeleniumApi:
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-gpu')
         options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--headless')
+        options.add_argument('--headless=new')
         options.add_argument('--start-maximized')
         result = False
 
